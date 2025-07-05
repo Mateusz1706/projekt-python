c06a5f824d43cafcda95b8302b1e8105ab4de646
Mateusz Stasiak <mateuszstasiak17@gmail.com>
2025-07-05 13:18:07 +0200

    feat: dodano informacje o autorze

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ def przywitanie():
 def project_info():
     print("To jest prosty projekt demonstracyjny do nauki systemu kontroli wersji Git.") # poprawiona treść
 
+def author_info():
+    print("Autor: Mateusz Stasiak")
+
 def show_menu():
     print("\n--- MENU ---")
     print("1. Powitanie")
     print("2. Informacje o projekcie")
     print("3. Zakończ")
+    print("4. Informacje o autorze")
+
 
 if __name__ == "__main__":
     while True:
@@ -24,5 +29,7 @@ if __name__ == "__main__":
         elif choice == "3":
             print("Do zobaczenia!")
             break
+	elif choice == "4":
+    	    author_info()
         else:
             print("Nieprawidłowy wybór, spróbuj ponownie.")
