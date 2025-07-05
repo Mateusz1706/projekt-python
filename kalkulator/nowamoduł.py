62528876de3a20a08e0448180fba8e30d5b4f2f4
Mateusz Stasiak <mateuszstasiak17@gmail.com>
2025-07-05 14:26:19 +0200

    fix: poprawiono walidację wejścia wg code review

--- kalkulator/nowamoduł.py
+++ kalkulator/nowamoduł.py
@@ -1,2 +1,10 @@
 def nowafunkcja(a, b):
-    return a + b
+    if a == "" or b == "":
+        raise ValueError("Puste dane wejściowe niedozwolone")
+    try:
+        a_int = int(a)
+        b_int = int(b)
+    except ValueError:
+        raise ValueError("Argumenty muszą być liczbami lub tekstem reprezentującym liczbę")
+    return a_int + b_int
+
